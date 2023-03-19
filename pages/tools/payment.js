e6d93b451b885d9c4a34ec250c7768a70098c086
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2023-03-19 07:49:25 +0600

    docker added

--- pages/tools/payment.js
+++ pages/tools/payment.js
@@ -1,28 +1,23 @@
 import React from 'react'
+import axios from "axios";
 
 export default function Payment() {
-    const handleSubmit = () => {
-        fetch("https://pay.edokanpay.com/checkout.php", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
-            },
-            body: new URLSearchParams({
-                api: "640c53e3f41df",
-                client: "640c53e400f4f",
-                secret: "47803468",
-                amount: 200,
-                position: "https://ctafsiras.vercel.app",
-                success_url: "https://ctafsiras.vercel.app/success",
-                cancel_url: "https://ctafsiras.vercel.app/cancel",
-                cus_name: "Tafsir",
-                cus_email: "tafsr@mail.com"
-            })
-        })
-            .then(response => response.text())
-            .then(result => console.log(result))
-            .catch(error => console.log('error', error));
+    const handleSubmit = async () => {
+        const data = "api=640c53e3f41df&client=640c53e400f4f&secret=47803468&position=https%3A%2F%2Fctafsiras.vercel.app&amount=33&success_url=https%3A%2F%2Fctafsiras.vercel.app&cancel_url=https%3A%2F%2Fctafsiras.vercel.app&cus_name=fsdf&cus_email=cjkj%40lkfjs.com";
 
+        const xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === this.DONE) {
+                console.log(this.responseText);
+            }
+        });
+
+        xhr.open("POST", "https://pay.edokanpay.com/checkout.php");
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+        xhr.send(data);
     }
     return (
         <div>
