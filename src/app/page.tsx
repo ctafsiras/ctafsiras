8b04a4aa6ae38f539721a4ce505a83e76cb5b02a
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2024-11-02 21:57:57 +0600

    updated leetCode api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,26 @@ import { Contact } from "@/components/contact";
 import LeetCode from "@/components/leetCode";
 export default async function Home() {
   const data = await fetch(
-    "https://leetcode-stats-api.herokuapp.com/ctafsiras",
+    "https://leetcode-api-faisalshohag.vercel.app/ctafsiras",
     {
       cache: "no-store",
     }
   );
-  const updatedStats = await data.json();
+  const res = await data.json();
+  const updatedStats = {
+    totalSolved: res.totalSolved,
+    totalQuestions: res.totalQuestions,
+    easySolved: res.easySolved,
+    totalEasy: res.totalEasy,
+    mediumSolved: res.mediumSolved,
+    totalMedium: res.totalMedium,
+    hardSolved: res.hardSolved,
+    totalHard: res.totalHard,
+    acceptanceRate: parseFloat(res.acceptanceRate.toFixed(4)),
+    contributionPoints: res.contributionPoint,
+    reputation: res.reputation,
+    ranking: res.ranking,
+  };
   return (
     <div className="container mx-auto px-4">
       <Hero />
